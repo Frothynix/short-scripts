b8bff5b821578b75202184ea8809c2312e66d27b
froth <froth@optiplex_server>
2020-03-28 18:36:48 +0000

    add removal of old world directory

--- newbackup.sh
+++ newbackup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#Copies world files to another folder
+#This script copies and creates a tarball of the minecraft server world on the backup disk
 DATE=$(date)
-FILENAME=$(date +%F)
+FILENAME=$(date +%F) #Date format YYYY-MM-DD
 cp -r /home/froth/minecraft/world /home/froth/backup/mcbackups/$FILENAME
 cd /home/froth/backup/mcbackups/
 tar -czvf $FILENAME.tar.gz $FILENAME
+rm -r /home/froth/backup/mcbackups/$FILENAME
 echo Backup complete on: $DATE >> /home/froth/backup/mcbackups/log
